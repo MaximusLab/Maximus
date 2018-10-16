36ecf7f58e4236513964302775ff7aec362f4da5
Leo <leo.on.the.earth@gmail.com>
2018-10-17 00:54:12 +0800

    Update phpdoc

--- src/Composer/ScriptHandler.php
+++ src/Composer/ScriptHandler.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the Maximus package.
  *
- * (c) Leo Tsun <leo.on.the.earth@gmail.com>
+ * (c) Leo <leo.on.the.earth@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
