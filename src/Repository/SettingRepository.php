4a3f2f9b19873320c50a7783f4c2e31a117a965a
Leo <leo.on.the.earth@gmail.com>
2018-10-21 18:36:27 +0800

    Add SettingRepository class

---
+++ src/Repository/SettingRepository.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Maximus\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Maximus\Entity\Setting;
+use Maximus\Setting\Settings;
+
+/**
+ * SettingRepository
+ */
+class SettingRepository extends EntityRepository
+{
+    /**
+     * @return Settings
+     */
+    public function getSettings()
+    {
+        $rows = $this->createQueryBuilder('setting')
+            ->select(['setting.key', 'setting.value'])
+            ->getQuery()
+            ->getArrayResult()
+        ;
+        $settings = [];
+
+        foreach ($rows as $row) {
+            $settings[$row['key']] = $row['value'];
+        }
+
+        return new Settings($settings);
+    }
+
+    /**
+     * @param Settings $settings
+     *
+     * @return bool
+     *
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function saveSettings(Settings $settings)
+    {
+        foreach ($settings->all() as $key => $value) {
+            $setting = $this->findOneBy(['key' => $key]);
+            $setting = $setting instanceof Setting ? $setting : new Setting($key);
+            $value = is_null($value) ? '' : $value;
+
+            $setting->setValue($value);
+
+            $this->_em->persist($setting);
+        }
+
+        $this->_em->flush();
+
+        return true;
+    }
+}
