e25aabca598cc7c52fafa76a995eb05788f0912c
Leo <leo.on.the.earth@gmail.com>
2018-10-21 18:38:01 +0800

    Add console pages

---
+++ src/Controller/Console/TagController.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * This file is part of the Maximus package.
+ *
+ * (c) Leo <leo.on.the.earth@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Maximus\Controller\Console;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/console/tag", name="console_tag_")
+ */
+class TagController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request)
+    {
+        $viewData = [
+
+        ];
+
+        return $this->render('console/tag/index.html.twig', $viewData);
+    }
+}
