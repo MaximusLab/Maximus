9fd012dc6c35b02b43a7194e63a27b1ce8e46090
Leo <leo.on.the.earth@gmail.com>
2018-10-22 23:59:50 +0800

    Update console author pages

--- src/Controller/Console/AuthorController.php
+++ src/Controller/Console/AuthorController.php
@@ -10,6 +10,10 @@
 
 namespace Maximus\Controller\Console;
 
+use Maximus\Entity\Author;
+use Maximus\Form\Type\AuthorType;
+use Maximus\Session\Flash;
+use Maximus\Twig\Breadcrumb;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,16 +26,78 @@ class AuthorController extends AbstractController
     /**
      * @Route("/", name="index")
      *
-     * @param Request $request
+     * @param Breadcrumb $breadcrumb
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction(Request $request)
+    public function indexAction(Breadcrumb $breadcrumb)
     {
-        $viewData = [
+        $authorRepo = $this->getDoctrine()->getRepository('Maximus:Author');
+        $authors = $authorRepo->findAll();
 
+        $breadcrumb
+            ->add('Home', $this->generateUrl('console_index'))
+            ->add('Authors', $this->generateUrl('console_author_index'), true)
+        ;
+
+        $viewData = [
+            'breadcrumb' => $breadcrumb,
+            'authors' => $authors,
         ];
 
         return $this->render('console/author/index.html.twig', $viewData);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     * @Route("/edit/{id}", name="edit", requirements={"id": "\d+"}, defaults={"id": 0})
+     *
+     * @param Request $request
+     * @param Breadcrumb $breadcrumb
+     * @param int $id Author ID
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function editAction(Request $request, Breadcrumb $breadcrumb, int $id = 0)
+    {
+        $author = 0 === $id ? new Author() : $this->getDoctrine()->getRepository('Maximus:Author')->find($id);
+
+        if (!$author instanceof Author) {
+            $this->addFlash(Flash::ERROR, 'Invalid author id!');
+
+            return $this->redirectToRoute('console_author_index');
+        }
+
+        $action = empty($author->getId()) ? 'Create' : 'Edit';
+        $form = $this->createForm(AuthorType::class, $author);
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+
+                $em->persist($author);
+                $em->flush();
+
+                $this->addFlash(Flash::SUCCESS, $action.' author (id: '.$author->getId().') successfully!');
+
+                return $this->redirectToRoute('console_author_index');
+            }
+        }
+
+        $breadcrumb
+            ->add('Home', $this->generateUrl('console_index'))
+            ->add('Authors', $this->generateUrl('console_author_index'))
+            ->add($action.' Author', '', true)
+        ;
+
+        $viewData = [
+            'breadcrumb' => $breadcrumb,
+            'action' => $action,
+            'form' => $form->createView(),
+        ];
+
+        return $this->render('console/author/edit.html.twig', $viewData);
     }
 }
---
+++ src/Form/Type/AuthorType.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * This file is part of the Maximus package.
+ *
+ * (c) Leo <leo.on.the.earth@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Maximus\Form\Type;
+
+use Maximus\Entity\Author;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class AuthorType
+ */
+class AuthorType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, ['label' => 'Name', 'attr' => ['placeholder' => 'Enter an author name']])
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Author::class,
+        ]);
+    }
+}
