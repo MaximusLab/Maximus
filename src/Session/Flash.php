3c11dadbaadb13f36d1b36ab8df565370d262163
Leo <leo.on.the.earth@gmail.com>
2018-10-22 23:58:57 +0800

    Add flash message blocks

---
+++ src/Session/Flash.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This file is part of the Maximus package.
+ *
+ * (c) Leo <leo.on.the.earth@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Maximus\Session;
+
+/**
+ * Class Flash
+ *
+ * @package Maximus\Session
+ */
+class Flash
+{
+    const SUCCESS = 'success';
+    const WARNING = 'warning';
+    const ERROR = 'error';
+}
