91720cefa2fb6b9f44561f85abaa30764e5d206b
Leo <leo.on.the.earth@gmail.com>
2018-10-24 00:37:55 +0800

    Add missing phpdoc

--- src/Repository/SettingRepository.php
+++ src/Repository/SettingRepository.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the Maximus package.
+ *
+ * (c) Leo <leo.on.the.earth@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Maximus\Repository;
 
