16fb4334911baa596d10b246311aeca55a9ef553
Leo <leo.on.the.earth@gmail.com>
2018-11-10 12:02:04 +0800

    Update tag pages

--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -10,6 +10,8 @@
 
 namespace Maximus\Controller;
 
+use Maximus\Entity\Article;
+use Maximus\Entity\Tag;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,26 +24,31 @@ class TagController extends AbstractController
      *
      * @Route("/tag/{tag}", name="tag")
      */
-    public function tagAction($tag)
+    public function tagAction(string $tag)
     {
+        $tag = $this->getDoctrine()->getRepository(Tag::class)->findOneBy(['title' => $tag]);
+
+        if (!$tag instanceof Tag) {
+            return $this->redirectToRoute('homepage');
+        }
+
+        $articles = $tag->getArticles();
+        $articlesPerYear = [];
+
+        /** @var Article $article */
+        foreach ($articles as $article) {
+            $year = $article->getPublishedYear();
+
+            if (!array_key_exists($year, $articlesPerYear)) {
+                $articlesPerYear[$year] = [];
+            }
+
+            $articlesPerYear[$year][] = $article;
+        }
+
         $viewData = [
             'tag' => $tag,
-            'articlesPerYear' => [
-                '2018' => [
-                    ['title' => '這是Game標tool123題 123The story, the most popular youth wear.123口嘗味123，她123多疼xyz你！', 'tags' => ['PHP', 'Paginator'], 'publishedAt' => new \DateTime('2018-02-18'), 'year' => '2018', 'month' => '02', 'day' => '18', 'englishTitle' => 'foo-bar'],
-                    ['title' => '圓滿這全程的寂寞，稍稍疏洩我的積愫，想起怎不可傷？', 'tags' => ['PHP', 'Symfony', 'Paginator'], 'publishedAt' => '2018-02-07', 'year' => '2018', 'month' => '02', 'day' => '18', 'englishTitle' => 'foo-bar'],
-                    ['title' => 'The best love story you\'ve ever read.', 'tags' => ['PHP'], 'publishedAt' => '2018-01-08', 'year' => '2018', 'month' => '02', 'day' => '18', 'englishTitle' => 'foo-bar'],
-                ],
-                '2017' => [
-                    ['title' => '這是標題 The story, the most popular youth wear.口嘗味，她多疼你！', 'tags' => ['PHP', 'Symfony', 'Paginator'], 'publishedAt' => new \DateTime('2017-02-18'), 'year' => '2018', 'month' => '02', 'day' => '18', 'englishTitle' => 'foo-bar'],
-                    ['title' => '圓滿這全程的寂寞，稍稍疏洩我的積愫，想起怎不可傷？', 'tags' => ['PHP', 'Talk', 'Paginator'], 'publishedAt' => '2017-02-07', 'year' => '2018', 'month' => '02', 'day' => '18', 'englishTitle' => 'foo-bar'],
-                ],
-                '2016' => [
-                    ['title' => '這是標題 The story, the most popular youth wear.口嘗味，她多疼你！', 'tags' => ['Symfony', 'Paginator'], 'publishedAt' => new \DateTime('2016-02-18'), 'year' => '2018', 'month' => '02', 'day' => '18', 'englishTitle' => 'foo-bar'],
-                    ['title' => '圓滿這全程的寂寞，稍稍疏洩我的積愫，想起怎不可傷？', 'tags' => ['PHP', 'Symfony', 'Paginator'], 'publishedAt' => '2016-02-07', 'year' => '2018', 'month' => '02', 'day' => '18', 'englishTitle' => 'foo-bar'],
-                    ['title' => 'The best love story you\'ve ever read.', 'tags' => ['PHP'], 'publishedAt' => '2016-01-08', 'year' => '2018', 'month' => '02', 'day' => '18', 'englishTitle' => 'foo-bar'],
-                ],
-            ],
+            'articlesPerYear' => $articlesPerYear,
         ];
 
         return $this->render('@theme/tag.html.twig', $viewData);
@@ -50,18 +57,22 @@ class TagController extends AbstractController
     /**
      * @return \Symfony\Component\HttpFoundation\Response
      *
-     * @Route("/tag/list", name="tags")
+     * @Route("/tags", name="tags")
      */
     public function listAction()
     {
+        $tags = $this->getDoctrine()->getRepository(Tag::class)->findAll();
+
         $viewData = [
-            'tags' => [
-                ['title' => 'PHP', 'description' => 'PHP related articles'],
-                ['title' => 'Paginator', 'description' => 'PHP related articles'],
-                ['title' => 'Symfony', 'description' => 'Symfony related articles'],
-                ['title' => 'Talk', 'description' => 'Some talk for life'],
-            ],
+            'tags' => [],
         ];
+
+        foreach ($tags as $tag) {
+            $viewData['tags'][] = [
+                'tag' => $tag,
+                'articleCount' => count($tag->getArticles()),
+            ];
+        }
 
         return $this->render('@theme/tags.html.twig', $viewData);
     }
