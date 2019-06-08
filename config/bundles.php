0dd72bc35a2848678e7150e6a818c85a49031e74
Leo <leo.on.the.earth@gmail.com>
2019-06-08 23:34:10 +0800

    Add security support for console pages

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
